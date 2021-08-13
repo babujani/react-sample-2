0d59c3d7260e9730f80644053cef5f963c75ae72
babujani <sbabujani@gmail.com>
2021-08-13 17:40:52 +0000

    commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const element = (
+  <div className="bg-container">
+    <h1 className="head-text">Congratulations</h1>
+    <div className="cong-card">
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png" />
+      <h1>Kiran V</h1>
+      <p>CBIT college Hyderabad</p>
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png" />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
